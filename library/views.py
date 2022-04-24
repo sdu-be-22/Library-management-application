0d25af1141ed1e207dc0b94e6c1ca68501cddd69
Ingkar <200103285@stu.sdu.edu.kz>
2022-04-24 15:34:59 +0600

    2 additional views added

--- library/views.py
+++ library/views.py
@@ -29,3 +29,46 @@ def adminclick_view(request):
         return HttpResponseRedirect('afterlogin')
     return render(request, 'library/adminclick.html')
 
+
+def adminsignup_view(request):
+    form = forms.AdminSignupForm()
+    if request.method == 'POST':
+        print(request.POST)
+        form = forms.AdminSignupForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            print(user)
+            user.set_password(user.password)
+            user.save()
+
+            my_admin_group = Group.objects.get_or_create(name='ADMIN')
+            print(my_admin_group[0].user_set.values())
+            my_admin_group[0].user_set.add(user)
+
+            return HttpResponseRedirect('adminlogin')
+    return render(request, 'library/adminsignup.html', {'form': form})
+
+
+def studentsignup_view(request):
+    form1 = forms.StudentUserForm()
+    form2 = forms.StudentExtraForm()
+    mydict = {'form1': form1, 'form2': form2}
+    if request.method == 'POST':
+        form1 = forms.StudentUserForm(request.POST)
+        form2 = forms.StudentExtraForm(request.POST)
+        if form1.is_valid() and form2.is_valid():
+            user = form1.save()
+            user.set_password(user.password)
+            user.save()
+            f2 = form2.save(commit=False)
+            f2.user = user
+            user2 = f2.save()
+
+            my_student_group = Group.objects.get_or_create(name='STUDENT')
+            my_student_group[0].user_set.add(user)
+
+        return HttpResponseRedirect('studentlogin')
+    return render(request, 'library/studentsignup.html', context=mydict)
+
+
+
